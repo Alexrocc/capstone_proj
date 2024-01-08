fab86f2adf59f747b3fc0b77d6fd67834b12dcb9
Alexrocc <alerocc70@gmail.com>
2024-01-08 15:34:38 +0100

    build start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreComponent } from './components/store/store.component';
+import { LibraryComponent } from './components/library/library.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoreComponent,
+    LibraryComponent,
+    ProfileComponent,
+    NavbarComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule
