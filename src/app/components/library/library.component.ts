767a11ecbf2ab8b91e15de91207b6448300d5acc
Alexrocc <alerocc70@gmail.com>
2024-01-18 18:48:35 +0100

    data-transfer almost complete

--- src/app/components/library/library.component.ts
+++ src/app/components/library/library.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/auth/auth-data';
+import { SteamService } from 'src/app/services/steam.service';
 
 @Component({
   selector: 'app-library',
   templateUrl: './library.component.html',
-  styleUrls: ['./library.component.scss']
+  styleUrls: ['./library.component.scss'],
 })
 export class LibraryComponent implements OnInit {
+  userId!: number;
+  currentUser!: User;
+  currentUserLibrary!: object[] | undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private steamSrv: SteamService) {
+    let user = localStorage.getItem('user');
+    if (user) {
+      let parsedUser = JSON.parse(user);
+      this.userId = parsedUser.user.id;
+    }
   }
 
+  ngOnInit(): void {
+    this.steamSrv.getUser(this.userId).subscribe((res) => {
+      if (res) {
+        this.currentUser = res;
+        this.currentUserLibrary = res.wishlist;
+      } else {
+        throw new Error();
+      }
+    });
+  }
 }
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/auth/auth-data';
+import { Steam } from 'src/app/interfaces/steam';
+import { SteamService } from 'src/app/services/steam.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  userId!: number;
+  currentUser!: User;
+  currentUserWishlist!: object[] | undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private steamSrv: SteamService) {
+    let user = localStorage.getItem('user');
+    if (user) {
+      let parsedUser = JSON.parse(user);
+      this.userId = parsedUser.user.id;
+    }
   }
 
+  ngOnInit(): void {
+    this.steamSrv.getUser(this.userId).subscribe((res) => {
+      if (res) {
+        this.currentUser = res;
+        this.currentUserWishlist = res.wishlist;
+      } else {
+        throw new Error();
+      }
+    });
+  }
 }
