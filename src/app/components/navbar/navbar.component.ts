6bbf3f92fed7dc5ffc748c605ae6fc3a1f597bc6
Alexrocc <alerocc70@gmail.com>
2024-01-28 19:33:46 +0100

    store layout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthData } from 'src/app/auth/auth-data';
 import { AuthService } from 'src/app/auth/auth.service';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
